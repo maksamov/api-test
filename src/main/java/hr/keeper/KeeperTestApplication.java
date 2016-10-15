b1b6b0e8e419737e7db8572b2b6ac0299e7138a5
Maksamov <marin.aksamovic@gmail.com>
2016-10-15 12:32:34 +0200

    first commit

---
+++ src/main/java/hr/keeper/KeeperTestApplication.java
@@ -0,0 +1,12 @@
+package hr.keeper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KeeperTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KeeperTestApplication.class, args);
+	}
+}
